5bc3c81938b84a338e4563a2b6f8b3cab4e8f261
toragames123 <fitora48@gmail.com>
2026-08-21 22:54:05 +0900

    Delete netlify/functions directory

--- netlify/functions/netlify/functions/netlify/functions/netlify/functions/health.js
+++
@@ -1,6 +0,0 @@
-const { json, preflight } = require("./_lib/http");
-
-exports.handler = async (event) => {
-  if (event.httpMethod === "OPTIONS") return preflight();
-  return json(200, { ok: true });
-};
